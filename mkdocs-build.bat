89734297efba2a368f3423d7ab9d06c09a7bd859
vikasburman <vikasburman@yahoo.com>
2014-11-23 14:41:09 +0530

    ...

--- mkdocs-build.bat
+++ mkdocs-build.bat
@@ -1,5 +1,12 @@
 @ECHO OFF
 mkdocs build --clean
+del "../../blackhawk/gh-pages/*.*" /S /A:-H
+rd "../../blackhawk/gh-pages/css" /S /Q
+rd "../../blackhawk/gh-pages/fonts" /S /Q
+rd "../../blackhawk/gh-pages/img" /S /Q
+rd "../../blackhawk/gh-pages/js" /S /Q
+rd "../../blackhawk/gh-pages/license" /S /Q
+xcopy site "../../blackhawk/gh-pages" /E
 start http://127.0.0.1:8000/
 mkdocs serve
 pause
